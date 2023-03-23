2fe63dabaa4e63ba104fd05f25b312654099a3d9
jkort <jortealv@myuax.com>
2023-03-23 19:26:57 +0100

    Metodos_generarNumeroPieIntentoDeClasePrincipal

--- src/main/java/aplicacion/Principal.java
+++ src/main/java/aplicacion/Principal.java
@@ -1,4 +1,14 @@
 package aplicacion;
 
+import mates.Matematicas;
+
 public class Principal {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Debes proporcionar un argumento que indique el número de pasos para generar una estimación de PI.");
+            return;
+        }
+
+        System.out.println("El número PI es " + Matematicas.generarNumeroPi(Long.parseLong(args[0])));
+    }
 }
--- src/main/java/mates/Matematicas.java
+++ src/main/java/mates/Matematicas.java
@@ -1,4 +1,26 @@
 package mates;
 
+import java.util.Random;
+
 public class Matematicas {
+
+    /**
+     * Genera una aproximación al número pi mediante el método de
+     * Montecarlo. El parámetro ‘pasos‘ indica el número de puntos
+     * generado.
+     */
+    public static double generarNumeroPi(long pasos) {
+        Random random = new Random();
+        long puntosDentroCirculo = 0;
+
+        for (long i = 0; i < pasos; i++) {
+            double x = random.nextDouble();
+            double y = random.nextDouble();
+            if (x*x + y*y <= 1.0) {
+                puntosDentroCirculo++;
+            }
+        }
+
+        return 4.0 * puntosDentroCirculo / pasos;
+    }
 }
